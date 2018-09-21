1c080f67c8e494902e4fa60c4bf235908358d715
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-21 20:53:11 +0000

    change file name

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,6 @@ require 'open-uri'
 require 'pry'
 
 require_relative '../lib/rest_full/version'
-require_relative '../lib/rest_full/restfull_cli'
+require_relative '../lib/rest_full/rest_full_cli'
 require_relative '../lib/rest_full/restaurant'
-require_relative '../lib/rest_full/restfull_scraper'
+require_relative '../lib/rest_full/rest_full_scraper'
