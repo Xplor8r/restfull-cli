0f07c4a7907578e62fe5fd6745b4b556432d3bb1
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-20 18:28:45 -0700

    require relative added to enviornment

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,8 @@ require 'nokogiri'
 require 'open-uri'
 
 require 'pry'
+
+require_relative '../lib/rest_full/version'
+require_relative '../lib/rest_full/restfull_cli'
+require_relative '../lib/rest_full/restaurant'
+require_relative '../lib/rest_full/restaurant_scraper'
