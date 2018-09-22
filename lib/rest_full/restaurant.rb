be95bd726d895196ce3f60688cd72badb014be74
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-22 08:49:31 -0700

    Update restaurant.rb

--- lib/rest_full/restaurant.rb
+++ lib/rest_full/restaurant.rb
@@ -1,14 +1,24 @@
 
 class Restfull::Restaurant
   attr_accessor :name, :location, :cuisines, :more_info
+  @@all = []
+  
+  def initialize
+    @@all << self
+  end
   
   def self.top_pick
-    restaurants = Restfull::Scraper.scrape_page
+    @@all = Restfull::Scraper.scrape_page
+    restaurant = @@all.shift
     top_pick = self.new
-    top_pick.name = restaurants.first[:name]
-    top_pick.location = restaurants.first[:location]
-    top_pick.cuisines = restaurants.first[:cuisines]
-    top_pick.more_info = restaurants.first[:more_info]
+    top_pick.name = restaurant[:name]
+    top_pick.location = restaurant[:location]
+    top_pick.cuisines = restaurant[:cuisines]
+    top_pick.more_info = restaurant[:more_info]
     top_pick
   end
+  
+  def all
+    @@all
+  end  
 end
