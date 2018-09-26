f29da330e80065e60c01e1537d38c252724397b1
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-26 09:56:44 -0700

    Update rest_full_scraper.rb

--- lib/rest_full/rest_full_scraper.rb
+++ lib/rest_full/rest_full_scraper.rb
@@ -1,5 +1,7 @@
 
 class Restfull::Scraper
+  attr_accessor :name, :location, :cuisines, :more_info
+  @@index = 0
   
   def self.scrape_page
     doc = Nokogiri::HTML(open("https://www.laweekly.com/restaurants/guide"))
@@ -17,5 +19,23 @@ class Restfull::Scraper
       end
     end
     scraped_restaurants
+  end
+
+  def self.top_pick
+    list_pick = self.scrape_page
+    
+      restaurant = list_pick[@@index]
+      top_pick = self.new
+      if restaurant == nil
+        puts "Sorry, I don't have any more suggestions."
+        abort
+      else
+        top_pick.name = restaurant[:name]
+        top_pick.location = restaurant[:location]
+        top_pick.cuisines = restaurant[:cuisines]
+        top_pick.more_info = restaurant[:more_info]
+        @@index += 1
+        top_pick
+      end
   end
 end
