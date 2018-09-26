6182d35bba2268bf71f857999150629bd2fcf6cd
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-26 10:01:40 -0700

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,4 @@ require 'pry'
 
 require_relative '../lib/rest_full/version'
 require_relative '../lib/rest_full/rest_full_cli'
-require_relative '../lib/rest_full/restaurant'
 require_relative '../lib/rest_full/rest_full_scraper'
