4cf22c2a67b8411f3682f7110a659fb797e6c826
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-27 10:36:09 -0700

    Update rest_full_scraper.rb

--- lib/rest_full/rest_full_scraper.rb
+++ lib/rest_full/rest_full_scraper.rb
@@ -3,23 +3,19 @@ class Restfull::Scraper
   
   def self.scrape_page
     doc = Nokogiri::HTML(open("https://www.laweekly.com/restaurants/guide"))
-
+    restaurants = []
     doc.css("div.result-set ul").each do |restaurant|
       restaurant.css("li").each do |info|
         if info.css("div.location a").text != ""
-          new_restaurant = Restfull::Restaurant.new
-          new_restaurant.name = info.css("div.location a").text
-          new_restaurant.location = info.css("div.address").text
-          new_restaurant.cuisines = info.css("div.cuisines").text
-          new_restaurant.more_info = "https://www.laweekly.com" + info.css("div.location a").attr("href").value
+          restaurants << {
+          :name => info.css("div.location a").text,
+          :location => info.css("div.address").text,
+          :cuisines => info.css("div.cuisines").text,
+          :more_info => "https://www.laweekly.com" + info.css("div.location a").attr("href").value
+          }
         end
       end
     end
-  end
-  
-  def self.scrape_info_page
-    info_doc = Nokogiri::HTML(open(new_restaurant.more_info))
-    new_restaurant.name = info_doc.css("h1").text
-    new_restaurant.description = info_doc.css(".description").text
+    restaurants
   end
 end
