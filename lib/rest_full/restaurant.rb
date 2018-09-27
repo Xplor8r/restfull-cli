afd670853f95a32b084397991ae6e40b9194a1b5
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-27 10:47:12 -0700

    Update restaurant.rb

--- lib/rest_full/restaurant.rb
+++ lib/rest_full/restaurant.rb
@@ -2,8 +2,8 @@ class Restfull::Restaurant
   attr_accessor :name, :location, :cuisines, :more_info
   @@all = []
   
-  def initialize(restaurants)
-    restaurants.each { |key, value| self.send("#{key}=", value) }
+  def initialize(restaurant)
+    restaurant.each { |key, value| self.send("#{key}=", value) }
     @@all << self
   end
   
@@ -11,4 +11,7 @@ class Restfull::Restaurant
     @@all 
   end
   
+  def self.create_restaurant(restaurants)
+    restaurants.each { |restaurant| self.new(restaurant) }
+  end
 end
